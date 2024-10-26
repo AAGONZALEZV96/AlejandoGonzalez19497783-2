9b270b8bbb8a7c40382e9953db33c9e46492cb3d
AAGONZALEZV96 <aa.gonzalezv96@gmail.com>
2024-10-26 09:34:51 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AlejandoGonzalez19497783-2',
+  webDir: 'www'
+};
+
+export default config;
